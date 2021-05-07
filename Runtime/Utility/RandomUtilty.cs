533053694ec0a812844168c1e805ff9879b31f49
MSI\OobaHiroya <hryooba@gmail.com>
2021-05-07 18:28:01 +0900

    [Add] RandomUtility

---
+++ Runtime/Utility/RandomUtilty.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HRYooba.Library
+{
+    public static class RandomUtility
+    {
+        public static Vector3 Vector3Range(Vector3 min, Vector3 max)
+        {
+            return new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+        }
+    }
+}
