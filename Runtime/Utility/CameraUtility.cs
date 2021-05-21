61902febc3a36b9c2ff650b72a23ccdb0c4bb275
MSI\OobaHiroya <hryooba@gmail.com>
2021-05-21 19:56:12 +0900

    [add] CameraUtilityを追加しました

---
+++ Runtime/Utility/CameraUtility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CameraUtility
+{
+    public static float HorizontalToVerticalFOV(float horizontalFOV, float aspect)
+    {
+        return Mathf.Rad2Deg * 2 * Mathf.Atan(Mathf.Tan((horizontalFOV * Mathf.Deg2Rad) / 2f) / aspect);
+    }
+}
