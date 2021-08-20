94eeb6c5b99ab34d5710e1295c0dda6afe05fe3d
1-10 Ooba RazerBlade <ooba@1-10.com>
2021-08-20 19:12:02 +0400

    [add] IEnumerableExtensionに関数実行する拡張追加

--- Runtime/Library/Extensions/IEnumerableExtension.cs
+++ Runtime/Library/Extensions/IEnumerableExtension.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public static class IEnumerableExtension
 {
     // 配列データのデバッグ時の確認用にストリング変換
-    public static string ToResult<TSource>(this IEnumerable<TSource> source)
+    public static string ToResult<T>(this IEnumerable<T> source)
     {
         return "{" + string.Join(", ", source) + "}";
+    }
+
+    // 関数実行
+    public static IEnumerable<T> Do<T>(this IEnumerable<T> source, System.Action<T> action)
+    {
+        return source;
     }
 }
