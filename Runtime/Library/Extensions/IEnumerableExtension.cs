3b33edf7c5843648d96493d650333cf0bc1b6391
1-10 Ooba RazerBlade <ooba@1-10.com>
2021-08-20 19:25:48 +0400

    [fix] IEnumerableExtension のバグ修正

--- Runtime/Library/Extensions/IEnumerableExtension.cs
+++ Runtime/Library/Extensions/IEnumerableExtension.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public static class IEnumerableExtension
 {
-    // 配列データのデバッグ時の確認用にストリング変換
     public static string ToResult<T>(this IEnumerable<T> source)
     {
         return "{" + string.Join(", ", source) + "}";
     }
 
-    // 関数実行
-    public static IEnumerable<T> Do<T>(this IEnumerable<T> source, System.Action<T> action)
+    public static void ForEach<T>(this IEnumerable<T> source, System.Action<T> action)
     {
-        return source;
+        foreach (var item in source)
+        {
+            action(item);
+        }
     }
 }
