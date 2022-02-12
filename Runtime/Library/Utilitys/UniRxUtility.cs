820653b3d4ca5c7cd9579bc5d421559f4dd9ba58
1-10 Ooba RazerBlade <ooba@1-10.com>
2022-02-13 05:35:03 +0900

    [add] UniRxUitlity追加

--- Runtime/Library/Utilitys/UniRxUtility.cs
+++ Runtime/Library/Utilitys/UniRxUtility.cs
@@ -4,9 +4,9 @@ using UniRx;
 
 namespace HRYooba.Library
 {
-    public static class UnirxExtension
+    public static class UniRxUtility
     {
-        public static IObservable<Unit> EitherTimerOrUnitObservable(this IObservable<Unit> self, float time, IObservable<Unit> unitObservable)
+        public static IObservable<Unit> EitherTimerOrUnitObservable(float time, IObservable<Unit> unitObservable)
         {
             return Observable.Amb(Observable.Timer(TimeSpan.FromSeconds(60.0f)).AsUnitObservable(), unitObservable);
         }
