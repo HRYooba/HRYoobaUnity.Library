fd9d951e4265c6931eefab9d1d5648d1a51b295d
1-10 Ooba RazerBlade <ooba@1-10.com>
2022-03-24 22:23:32 +0900

    [add] textureloader

---
+++ Runtime/Library/TextureLoader.cs
@@ -0,0 +1,82 @@
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public static class TextureLoader
+{
+    public static Texture2D[] LoadTextures(string directPath)
+    {
+        if (Directory.Exists(directPath))
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directPath);
+            var fileInfos = directoryInfo.GetFiles("*").Where(_ => _.Name.EndsWith(".jpg") || _.Name.EndsWith(".png"));
+
+            if (fileInfos.Count() > 0)
+            {
+                var textures = new Texture2D[fileInfos.Count()];
+                for (var i = 0; i < fileInfos.Count(); i++)
+                {
+                    var path = directPath + "\\" + fileInfos.ToArray()[i].Name;
+                    if (File.Exists(path))
+                    {
+                        var bytes = File.ReadAllBytes(path);
+                        textures[i] = new Texture2D(2, 2);
+                        textures[i].LoadImage(bytes);
+                        Debug.Log("Load texture: " + path);
+                    }
+                }
+
+                return textures;
+            }
+            else
+            {
+                Debug.LogError("Not exists textures: " + directPath);
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogError("Not exists path: " + directPath);
+            return null;
+        }
+    }
+
+    public static async Task<Texture2D[]> LoadTexturesAsync(string directPath, CancellationToken cancellationToken)
+    {
+        if (Directory.Exists(directPath))
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directPath);
+            var fileInfos = directoryInfo.GetFiles("*").Where(_ => _.Name.EndsWith(".jpg") || _.Name.EndsWith(".png"));
+
+            if (fileInfos.Count() > 0)
+            {
+                var textures = new Texture2D[fileInfos.Count()];
+                for (var i = 0; i < fileInfos.Count(); i++)
+                {
+                    var path = directPath + "\\" + fileInfos.ToArray()[i].Name;
+                    if (File.Exists(path))
+                    {
+                        var bytes = await File.ReadAllBytesAsync(path, cancellationToken);
+                        textures[i] = new Texture2D(2, 2);
+                        textures[i].LoadImage(bytes);
+                        Debug.Log("Load texture: " + path);
+                    }
+                }
+
+                return textures;
+            }
+            else
+            {
+                Debug.LogError("Not exists textures: " + directPath);
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogError("Not exists path: " + directPath);
+            return null;
+        }
+    }
+}
