da5630947e068f1e02057e880db0e59db8c0d602
1-10 Ooba RazerBlade <ooba@1-10.com>
2023-04-07 16:36:18 +0900

    [update] PlayableDirectorExtension更新

--- Runtime/Library/Extensions/PlayableDirectorExtension.cs
+++ Runtime/Library/Extensions/PlayableDirectorExtension.cs
@@ -1,5 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -9,6 +11,45 @@ namespace HRYooba.Library
 {
     public static class PlayableDirectorExtension
     {
+        public static void RewindPlay(this PlayableDirector playableDirector)
+        {
+            playableDirector.time = 0.0;
+            playableDirector.Play();
+        }
+
+        public static void Rewind(this PlayableDirector playableDirector)
+        {
+            playableDirector.time = 0.0;
+            playableDirector.Stop();
+        }
+
+        public static UniTask PlayAsync(this PlayableDirector playableDirector, CancellationToken cancellationToken)
+        {
+            playableDirector.Play();
+            return UniTask.WaitUntil(() => playableDirector.state == PlayState.Playing, cancellationToken: cancellationToken);
+        }
+
+        public static UniTask RewindPlayAsync(this PlayableDirector playableDirector, CancellationToken cancellationToken)
+        {
+            playableDirector.RewindPlay();
+            return UniTask.WaitUntil(() => playableDirector.state == PlayState.Playing, cancellationToken: cancellationToken);
+        }
+
+        public static IObservable<PlayableDirector> PlayedAsObservable(this PlayableDirector playableDirector)
+        {
+            return Observable.FromEvent<PlayableDirector>(h => playableDirector.played += h, h => playableDirector.played -= h);
+        }
+
+        public static IObservable<PlayableDirector> PausedAsObservable(this PlayableDirector playableDirector)
+        {
+            return Observable.FromEvent<PlayableDirector>(h => playableDirector.paused += h, h => playableDirector.paused -= h);
+        }
+
+        public static IObservable<PlayableDirector> StoppedAsObservable(this PlayableDirector playableDirector)
+        {
+            return Observable.FromEvent<PlayableDirector>(h => playableDirector.stopped += h, h => playableDirector.stopped -= h);
+        }
+
         public static void BindAllActivationTrackToThisInactivation(this PlayableDirector playableDirector)
         {
             var tracks = (playableDirector.playableAsset as TimelineAsset).GetOutputTracks();
